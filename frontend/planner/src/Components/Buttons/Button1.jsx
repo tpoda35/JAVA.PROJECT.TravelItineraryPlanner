973bbf4b8d0d2f5b964006c3ee9506f22cb79d49
tpoda <tpoda35@gmail.com>
2025-06-05 14:43:22 +0200

    Fixed navbar colors, element placement.

--- frontend/planner/src/Components/Buttons/Button1.jsx
+++ frontend/planner/src/Components/Buttons/Button1.jsx
@@ -1,7 +1,7 @@
 
 export default function Button1({text}) {
     return (
-        <button className="button-1 button-margin">
+        <button className="button-highlighted button-margin">
             {text}
         </button>
     );
